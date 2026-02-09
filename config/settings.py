88988897c4bb3e830dc43079bedc109fb78bc5d7
Prasoon <ariesvoldigord@gmail.com>
2026-02-09 20:55:17 +0530

    Fix: add STATIC_ROOT for Render collectstatic

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^qxwwrwqic^ao$y3(ve^w#k*h^c2b4_enav_vyamrwkitpc*4k'
+SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG") == "True"
+
 
 ALLOWED_HOSTS = []
 
@@ -116,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT= BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     BASE_DIR / 'portfolio' / 'static',
 ]
